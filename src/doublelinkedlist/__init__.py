6202948be5d05c9250bd11502149f73a3cf232bf
Lachlan Mayes <lachlanmayes@hotmail.com>
2025-06-23 08:54:01 +1000

    adding changes to to src

---
+++ src/doublelinkedlist/__init__.py
@@ -0,0 +1,7 @@
+"""A doubly linked list implementation in Python."""
+
+from .node import Node
+from .double_linked_list import Double_Linked_List
+
+__version__ = "0.1.0"
+__all__ = ["Node", "Double_Linked_List"]
